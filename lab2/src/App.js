0feae35f5f72510f993fd49edcd956ff57d2ec07
Ebbe Arildsson <ebbe.arildsson@gmail.com>
2023-09-14 21:53:23 +0200

    ✨ Basic picker

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -6,18 +6,12 @@ import ComposeSalad from './ComposeSalad'
 function App() {
   const extras = Object.keys(inventory).filter(name => inventory[name].extra);
   return (
+    
     <div className="container py-4">
       <header className="pb-3 mb-4 border-bottom">
         <span className="fs-4">Min egen salladsbar</span>
       </header>
-
-      <div className="continer col-12">
-        <div className="row h-200 p-5 bg-light border rounded-3">
-          <h2>Välj innehållet i din sallad</h2>
-          {extras.map(name => <div key={name} className="col-4">{name}</div>)}
-        </div>
-      </div>
-
+      <ComposeSalad inventory={inventory}/>
       <footer className="pt-3 mt-4 text-muted border-top">
         EDAF90 - webprogrammering
       </footer>
@@ -26,3 +20,14 @@ function App() {
 }
 
 export default App;
+
+
+function chooseExtras(extras) {
+  return <div className="continer col-12">
+    <div className="row h-200 p-5 bg-light border rounded-3">
+      <h2>Välj innehållet i din sallad</h2>
+      {extras.map(name => <div key={name} className="col-4">{name}</div>)}
+    </div>
+  </div>;
+}
+
--- lab2/src/ComposeSalad.js
+++ lab2/src/ComposeSalad.js
@@ -1,15 +1,35 @@
 import { useState } from 'react';
 
-function ComposeSalad(props) {
-  const foundationList = Object.keys(props.inventory).filter(name => props.inventory[name].foundation);
+function ComposeSalad({inventory}) {
+  const foundationList = Object.keys(inventory).filter(name => inventory[name].foundation);
+  const proteinList = Object.keys(inventory).filter(name => inventory[name].protein);
+  const extraList = Object.keys(inventory).filter(name => inventory[name].extra);
+  const dressingList = Object.keys(inventory).filter(name => inventory[name].dressing);
   const [foundation, setFoundation] = useState('Pasta');
   const [extras, setExtra] = useState({ Bacon: true, Fetaost: true });
+  const [dressing, setDressing] = useState('Rhodeisland');
+  const [protein, setProtein] = useState('Kycklingfilé');
 
   return (
     <div className="continer col-12">
       <div className="row h-200 p-5 bg-light border rounded-3">
-        <h2>Välj bas</h2>
-        {foundationList.map(name => <div key={name} className="col-4">{name}</div>)}
+      <h5>Välj bas</h5>
+      <select className="form-select" aria-label="Default select example" value={foundation} onChange={(event) => setFoundation(event.target.value)}>
+        {foundationList.map(name => <option key={name} className="col-4" value={name}>{name}</option>)}
+      </select>
+      <h5>Välj protein</h5>
+      <select className="form-select" aria-label="Default select example" value={protein} onChange={(event) => setProtein(event.target.value)}>
+        {proteinList.map(name => <option key={name} className="col-4" value={name}>{name}</option>)}
+      </select>
+      <h5>Välj extra</h5>
+      <select className="form-select" aria-label="Default select example" value={extras} onChange={(event) => setExtra(event.target.value)}>
+        {extraList.map(name => <option key={name} className="col-4" value={name}>{name}</option>)}
+      </select>
+      <h5>Välj dressing</h5>
+      <select className="form-select" aria-label="Default select example" value={dressing} onChange={(event) => setDressing(event.target.value)}>
+        {dressingList.map(name => <option key={name} className="col-4" value={name}>{name}</option>)}
+      </select>
+      <button type="button" className="btn btn-primary">Lägg till sallad</button>
       </div>
     </div>
   );
