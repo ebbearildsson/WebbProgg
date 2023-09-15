212c30c6bd16502699826fb3542060df7e396467
Ebbe Arildsson <ebbe.arildsson@gmail.com>
2023-09-15 11:38:38 +0200

    💄 Better looking and responsive

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -3,14 +3,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 import inventory from './inventory.mjs';
 import ComposeSalad from './ComposeSalad'
 
-function App() {
+function App() { 
   return (
     <div className="container py-4">
       <header className="pb-3 mb-4 border-bottom">
         <span className="fs-4">Min egen salladsbar</span>
       </header>
       <div className="row h-200 p-5 bg-light border rounded-3">
-        Hello
+       <span className="fs-4">Varukorg</span>
       </div>
       <ComposeSalad inventory={inventory}/>
       <footer className="pt-3 mt-4 text-muted border-top">
--- lab2/src/ComposeSalad.js
+++ lab2/src/ComposeSalad.js
@@ -22,25 +22,28 @@ function ComposeSalad({inventory}) {
 
   return (
     <div className="continer col-12">
-      <div className="row h-200 p-5 bg-light border rounded-3">
-      <h5>Välj bas</h5>
-      <select className="form-select" aria-label="Default select example" value={foundation} onChange={(event) => setFoundation(event.target.value)}>
-        {foundationList.map(name => <option key={name} className="col-4" value={name}>{name}</option>)}
-      </select>
-      <h5>Välj protein</h5>
-      <select className="form-select" aria-label="Default select example" value={protein} onChange={(event) => setProtein(event.target.value)}>
-        {proteinList.map(name => <option key={name} className="col-4" value={name}>{name}</option>)}
-      </select>
-      <h5>Välj extra</h5>
-        {extraList.map(name => <span key={name}>
-            <input type="checkbox" className="btn-check" id={name[0]} autoComplete="off" onChange={handleCheck} value={name[0]}></input>
-            <label className="btn" htmlFor={name[0]}>{name[0]} ({name[1]} kr)</label>
-          </span>)}
-      <h5>Välj dressing</h5>
-      <select className="form-select" aria-label="Default select example" value={dressing} onChange={(event) => setDressing(event.target.value)}>
-        {dressingList.map(name => <option key={name} className="col-4" value={name}>{name}</option>)}
-      </select>
-      <button type="button" className="btn btn-primary">Lägg till sallad</button>
+      <div className="row h-200 p-5 bg-light border rounded-3 salad-maker">
+        <span className="fs-4">Ingridienser</span>
+        <h5>Välj bas</h5>
+        <select className="form-select" aria-label="Default select example" value={foundation} onChange={(event) => setFoundation(event.target.value)}>
+          {foundationList.map(name => <option key={name} className="col-4" value={name}>{name}</option>)}
+        </select>
+        <h5>Välj protein</h5>
+        <select className="form-select" aria-label="Default select example" value={protein} onChange={(event) => setProtein(event.target.value)}>
+          {proteinList.map(name => <option key={name} className="col-4" value={name}>{name}</option>)}
+        </select>
+        <h5>Välj extra</h5>
+        <div className="group">
+          {extraList.map(name => <span key={name}>
+              <input type="checkbox" className="btn-check" id={name[0]} autoComplete="off" onChange={handleCheck} value={name[0]}></input>
+              <label className="btn btn-outline-primary" htmlFor={name[0]}>{name[0]} ({name[1]} kr)</label>
+            </span>)}
+        </div>
+        <h5>Välj dressing</h5>
+        <select className="form-select" aria-label="Default select example" value={dressing} onChange={(event) => setDressing(event.target.value)}>
+          {dressingList.map(name => <option key={name} className="col-4" value={name}>{name}</option>)}
+        </select>
+        <button type="button" className="btn btn-primary">Lägg till sallad</button>
       </div>
     </div>
   );
